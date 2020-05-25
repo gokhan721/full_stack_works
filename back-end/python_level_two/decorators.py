dffc5d52c24198acff8abd4309b73a99e84f0c4f
gokhan721 <simsek.gokhan721@gmail.com>
2020-05-25 09:44:42 +0300

    memoization for fib

--- back-end/python_level_two/decorators.py
+++ back-end/python_level_two/decorators.py
@@ -33,6 +33,7 @@ print(x())
 
 # func decorators
 
+
 def new_decorator(func):
 
     def wrap_func():
@@ -42,19 +43,23 @@ def new_decorator(func):
 
     return wrap_func
 
+
 @new_decorator
 def func_needs_decorator():
     print("THIS FUNCTION NEEDS OF A DECORATOR")
 
 # @new_decorator => func_needs_decorator = new_decorator(func_needs_decorator)
 
+
 func_needs_decorator()
 
 # class decorators
 
+
 class decorator:
     def __init__(self, f):
         self.f = f
+
     def __call__(self):
         print(f"Decorating {self.f.__name__}")
         self.f()
@@ -66,3 +71,53 @@ def foo():
 
 
 foo()
+
+
+# memoization
+
+def memoize(f):
+    memo = {}
+
+    def helper(x):
+        print(f"x: {x}")
+        if x not in memo:
+            memo[x] = f(x)
+        return memo[x]
+    return helper
+
+
+@memoize
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+fib = fib(50)
+
+print(fib)
+
+
+class Memoize:
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.fn(*args)
+        return self.memo[args]
+
+@Memoize
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print(fib(7))
