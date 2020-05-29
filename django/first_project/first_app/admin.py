a8115e2880e9599e78833c82feef4c681b7a2c58
gokhan721 <simsek.gokhan721@gmail.com>
2020-05-29 15:25:42 +0300

    models and admin interface

--- django/first_project/first_app/admin.py
+++ django/first_project/first_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from first_app.models import *
 
 # Register your models here.
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("top_name",)
+
+
+class WebpageAdmin(admin.ModelAdmin):
+    list_display = ("topic", "name", "url")
+
+class AccessRecordAdmin(admin.ModelAdmin):
+    list_display = ("name", "date")
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Webpage, WebpageAdmin)
+admin.site.register(AccessRecord, AccessRecordAdmin)
