5f7e73d6b0ac0d29fc73af38b414a66ae3d94670
gokhan721 <simsek.gokhan721@gmail.com>
2020-05-29 17:53:15 +0300

    populating dummy data by Faker pypı

---
+++ django/first_project/populating_dummy_data.py
@@ -0,0 +1,47 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_project.settings")
+
+import django
+
+django.setup()
+
+## FAKER POP SCRIPTS
+
+import random
+from faker import Faker
+from first_app.models import *
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name = random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(N=5):
+
+    for entry in range(N):
+
+        # get the topic for entry
+        top = add_topic()
+
+        # Create the fate data for entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        # Create the new webpage entry
+        webpg = Webpage.objects.get_or_create(topic=top, url = fake_url, name = fake_name)[0]
+
+        # create a face access record for Webpage
+        acc_rec = AccessRecord.objects.get_or_create(name = webpg, date = fake_date)[0]
+
+
+if __name__ == "__main__":
+    print("Populating script")
+    populate(20)
+    print("Population completed")
