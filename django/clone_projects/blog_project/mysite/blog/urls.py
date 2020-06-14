cd58ecf5e0240a7f4500abc7072d61092f5e95a6
gokhan721 <simsek.gokhan721@gmail.com>
2020-06-14 22:41:47 +0300

    Css,js and login system

--- django/clone_projects/blog_project/mysite/blog/urls.py
+++ django/clone_projects/blog_project/mysite/blog/urls.py
@@ -3,7 +3,6 @@ from blog import views
 
 app_name = "blog"
 
-
 urlpatterns = [
     path('', views.PostListView.as_view(), name="post_list"),
     path('about/', views.AboutView.as_view(), name="about"),
@@ -12,4 +11,8 @@ urlpatterns = [
     path('post/<pk>/edit/', views.PostUpdateView.as_view(), name="post_edit"),
     path('post/<pk>/remove/', views.PostDeleteView.as_view(), name="post_remove"),
     path('drafts/', views.DraftListView.as_view(), name="post_draft_list"),
+    path('post/<pk>/comment/', views.add_comment_to_post, name= "add_comment_to_post"),
+    path('comment/<pk>/approve/', views.comment_approve, name="comment_approve"),
+    path('comment/<pk>/remove/',views.comment_removed, name="comment_removed"),
+    path('post/<pk>/publish', views.post_publish, name="post_publish"),
 ]
--- django/clone_projects/blog_project/mysite/blog/views.py
+++ django/clone_projects/blog_project/mysite/blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (TemplateView, ListView,
-                                DetailView, CreateView,
-                                UpdateView, DeleteView)
+                                  DetailView, CreateView,
+                                  UpdateView, DeleteView)
 from blog.models import Post, Comment
-from django.contrib.auth.mixins import LoginRequiredMixin # class-based view
-from django.contrib.auth.decorators import login_required # function_based view
+from django.contrib.auth.mixins import LoginRequiredMixin  # class-based view
+from django.contrib.auth.decorators import login_required  # function_based view
 from blog.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -58,4 +58,41 @@ class DraftListView(LoginRequiredMixin, ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__isnull = True).orderby("created_date")
+        return Post.objects.filter(published_date__isnull=True).orderby("created_date")
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, "blog/comment_form.html",{'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_removed(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect("post_detail", pk = post_pk)
