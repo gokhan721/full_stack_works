c8d7875ca7c0c44dbf0a42086c0f7574c8df7087
gokhan721 <simsek.gokhan721@gmail.com>
2020-07-30 19:03:13 +0300

    medium editor has been fixed.

--- django/clone_projects/blog_project/mysite/blog/forms.py
+++ django/clone_projects/blog_project/mysite/blog/forms.py
@@ -3,23 +3,21 @@ from blog.models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = Post
-        fields = ('author', 'title', 'text')
-
-    widgets = {
-        'title': forms.TextInput(attrs={'class': 'textinputclass'}),
-        'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
-    }
+        fields = ('author', 'title', 'text',)
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
+        }
 
 
 
 class CommentForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = Comment
-        fields = ('author', 'text')
-
-    widgets = {
-        'author': forms.TextInput(attrs={'class': 'textinputclass'}),
-        'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
-    }
+        fields = ('author', 'text',)
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
+        }
